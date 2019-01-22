ed2e37fe8a26afc77d334c62c1e6d05a1cd15c11
Noah Conroy <noahconroy@NoaBoston-MacBook-Pro.local>
2019-01-22 15:21:46 -0600

    Updates

--- LearningProject1/src/main/java/frc/robot/RobotMap.java
+++ LearningProject1/src/main/java/frc/robot/RobotMap.java
@@ -13,7 +13,8 @@ package frc.robot;
  * the wiring easier and significantly reduces the number of magic numbers
  * floating around.
  */
-public class RobotMap {
+public class RobotMap 
+{
   // For example to map the left and right motors, you could define the
   // following variables to use with your drivetrain subsystem.
   // public static int leftMotor = 1;
